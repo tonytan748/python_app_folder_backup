451f2dfb56dbe7a670d6a36d1c67eacb7b2534ab
tan yong <violin748@hotmail.com>
2014-05-16 01:53:42 +0000

    update

--- backup-2.py
+++ backup-2.py
@@ -3,13 +3,15 @@ import os
 import shutil
 import datetime
 # 设置待备份的源文件夹及存放备份文件的目标文件夹
-srcDir = os.path.abspath(r'\\192.168.30.4\Inventory Control Department\1. MATERIAL SYSTEM-1.0')
-
-a='Material System-1.0 - ' + datetime.datetime.now().strftime('%Y%m%d')
-
 material_backup_folder = os.path.abspath(r'D:\Material System-1.0')
 
+srcDir = os.path.abspath(r'\\192.168.30.4\Inventory Control Department\1. MATERIAL SYSTEM-1.0')
+a='Material System-1.0 - ' + datetime.datetime.now().strftime('%Y%m%d')
 dstDir=os.path.join(material_backup_folder,a)
+
+mansrcDir=os.path.abspath(r'W:\Daily Manpower Record (Monitoring)')
+b='MANPOWER RECORD - ' + datetime.datetime.now().strftime('%Y%m%d')
+manDir=os.path.join(material_backup_folder,b)
 
 # 目录递归拷贝函数
 def dir_copyTree(src, dst):
@@ -36,11 +38,37 @@ def dir_copyTree(src, dst):
 def dir_backup():
 	global srcDir
 	global dstDir
+	global mansrcDir
+	global manDir
 	print "Source Folder" + srcDir
 	print "Destination Folder" + dstDir
 	print "The copy  of the Document:"
 	dir_copyTree(srcDir, dstDir)
 	# 将此句注释则会一闪而过，方便自动备份
+	print "=========================================================="
+	print "========================================================"
+	print "======================================================"
+	print "===================================================="
+	print "=================================================="
+	print "================================================"
+	print "=============================================="
+	print "============================================"
+	print "=========================================="
+	print "========================================"
+	print "======================================"
+	print "===================================="
+	print "=================================="
+	print "================================"
+	print "============================"
+	print "=========================="
+	print "========================"
+	print "======================"
+	print "===================="
+	print "=================="
+	print "================"
+	print "=============="
+	print "============"
+	dir_copyTree(mansrcDir, manDir)
 	raw_input ("The backup is complete.")
 # 执行备份函数
 dir_backup()
